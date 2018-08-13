5f8fe5f7bccecba495f07eb79dfb7c1f9cbcd750
WebSEC.nl <rebitclown105@hotmail.com>
2018-08-13 10:59:20 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -4,16 +4,16 @@ import binascii
 from uuid import getnode as get_mac
 import socket
 mac = get_mac()
-license = (str(mac) + '-' + str(socket.gethostname()))
-
-hwid = base64.b64encode(license)
+plain = (str(mac) + '-' + str(socket.gethostname()))
+hwid = base64.b64encode(plain)
+license = binascii.hexlify(hwid).upper())[::-1]
 
 banner = ("######################################\n"
           "## HWID GENERATOR - BY JOEL A. OSSI ##\n"
           "######################################")
 print(banner)
 print("")
-print('[+] Your HWID: ' + str(binascii.hexlify(hwid).upper())[::-1])
-pyperclip.copy(str(binascii.hexlify(hwid).upper())[::-1])
+print('[+] Your HWID: ' + str(license)
+pyperclip.copy(str(license)
 print('[+] HWID Set to Clipbaord')
 raw_input("")
